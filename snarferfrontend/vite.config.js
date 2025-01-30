8f3ffebafdf077fefe0bf6a7a69fb25daa35b37e
mordini <yahewisthedevil@yahoo.com>
2025-01-29 22:27:54 -0500

    added any ip access in frontend

--- snarferfrontend/vite.config.js
+++ snarferfrontend/vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {host: '0.0.0.0'},
   plugins: [
     vue(),
     vueDevTools(),
